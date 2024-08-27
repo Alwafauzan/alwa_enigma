f1c6c92aea9fbe3d8b4c479d52f174863f3bf061
alwafauzan123 <alwafauzan123@gmail.com>
2024-08-27 21:51:45 +0700

    challenge robot maven

--- jdbc_nasabah/nasabah/src/config/DatabaseConfig.java
+++ jdbc_nasabah/nasabah/src/config/DatabaseConfig.java
@@ -8,13 +8,13 @@ public class DatabaseConfig {
 
 
     public static Connection getConnection() throws SQLException {
-        // String DB_URL = System.getenv("DB_URL");
-        // String DB_USER = System.getenv("DB_USER");
-        // String DB_PASSWORD = System.getenv("DB_PASSWORD");
+        String DB_URL = System.getenv("DB_URL");
+        String DB_USER = System.getenv("DB_USER");
+        String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
-        String DB_URL = "jdbc:postgresql://localhost:5433/coba_jdbc";
-        String DB_USER = "postgres";
-        String DB_PASSWORD = "myPassword";
+        // String DB_URL = "jdbc:postgresql://localhost:5433/coba_jdbc";
+        // String DB_USER = "postgres";
+        // String DB_PASSWORD = "myPassword";
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 }
---
+++ robot-runner/src/main/java/com/chellenge/robot/runner/MainRunner.java
@@ -0,0 +1,9 @@
+package com.chellenge.robot.runner;
+
+import com.chellenge.robot.console.RobotConsole;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        RobotConsole.main(args);  // Menjalankan aplikasi dari JAR
+    }
+}
